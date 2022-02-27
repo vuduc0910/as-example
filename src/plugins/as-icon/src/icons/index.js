f11f49c4f3458642b0503dde37c0e2577d6370a4
DucVV6 <p.fpt.duc.vovu@firstgroup.jp>
2022-02-27 20:06:51 +0700

    feat(icon) : add icon

--- src/plugins/as-icon/src/icons/index.js
+++ src/plugins/as-icon/src/icons/index.js
@@ -1,6 +1,24 @@
 import { replaceCard } from './replaceCard'
 import { freezeCard } from './freezeCard'
+import { account } from './account'
+import { credit } from './credit'
+import { directiveCard } from './directiveCard'
+import { gpay } from './gpay'
+import { logo } from './logo'
+import { pay } from './pay'
+import { payments } from './payments'
+import { spendLimit } from './spendLimit'
+import { transfer } from './transfer'
 export default {
-  replaceCard,
-  freezeCard,
+  'replace-card': replaceCard,
+  'freeze-card': freezeCard,
+  account,
+  credit,
+  'directive-card': directiveCard,
+  gpay,
+  logo,
+  pay,
+  payments,
+  spendLimit,
+  transfer,
 }
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <AsIcon name="freezeCard" />
+    <AsIcon name="freeze-card" />
+    <AsIcon name="logo" />
     <!-- <img :src="require('./card.svg')" /> -->
   </div>
 </template>
